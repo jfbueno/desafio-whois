f429701f250cdeb8044bafa7f187cd8766ac9355
Jéferson <buenojeferson@outlook.com>
2017-08-03 17:42:23 -0300

    spinner de loading ao fazer requisição

--- DesafioWhois/Scripts/app/mainController.js
+++ DesafioWhois/Scripts/app/mainController.js
@@ -7,18 +7,23 @@
 
         ctrl.urlBusca = '';
         ctrl.viewModel = null;
+        ctrl.erro = false;
+        ctrl.carregando = false;
+
         ctrl.buscar = _buscar;
         ctrl.atualizarCache = _atualizarCache;
-        ctrl.erro = false;
 
         function _buscar() {
             ctrl.erro = false;
+            ctrl.carregando = true;
 
             whoisApiService.buscarPorDominio(ctrl.urlBusca).then(function (response) {
                 ctrl.viewModel = response.data;
             }).catch(function (erro) {
                 ctrl.viewModel = null;
                 ctrl.erro = erro.data;
+            }).finally(function() {
+                ctrl.carregando = false;
             });
         }
 
@@ -27,12 +32,15 @@
                 return;
 
             ctrl.erro = false;
+            ctrl.carregando = true;
 
             whoisApiService.atualizarCache(ctrl.viewModel.Dado.Id).then(function (response) {
                 ctrl.viewModel = response.data;
             }).catch(function (erro) {
                 ctrl.erro = erro.data;
                 ctrl.viewModel = null;
+            }).finally(function() {
+                ctrl.carregando = false;
             });
         }
     }
